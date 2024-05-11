ce4fb1527f9c000a5c608e94f7840106a82a355f
asmcid <168562874+asmcid@users.noreply.github.com>
2024-05-11 22:44:47 +0700

    Update install.py

--- install.py
+++ install.py
@@ -4,12 +4,10 @@ from dotenv import set_key
 cloudflare_api_token = input("Enter Cloudflare API token➡️ ")
 bot_token = input("Enter Telegram Bot token➡️ ")
 account_id = input("Enter Cloudflare Account ID➡️ ")
-admin_user_id = input("Enter Admin User ID➡️ ")
 
 set_key('.env', 'CLOUDFLARE_API_TOKEN', cloudflare_api_token)
 set_key('.env', 'BOT_TOKEN', bot_token)
 set_key('.env', 'ACCOUNT_ID', account_id)
-set_key('.env', 'ADMIN_USER_ID', admin_user_id)
 set_key('.env', 'IP_API', 'https://raw.githubusercontent.com/2ri4eUI/CFW_Worker_Sub/main/ips.txt')
 
 with open('workertemp.txt', 'r') as file:
